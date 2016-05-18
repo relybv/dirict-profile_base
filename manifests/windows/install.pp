748169e77ea00c0fdd3ac6595045da527e537b80
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-18 18:51:12 +0000

    make monitor address dynamic

--- manifests/windows/install.pp
+++ manifests/windows/install.pp
@@ -9,17 +9,16 @@ class profile_base::windows::install {
   Package { provider => chocolatey, }
 
   # if monitor address is defined use it as syslogserver
-#  if $profile_base::monitor_address != undef {
+  if $profile_base::monitor_address != undef {
     include nxlog
     nxlog::input {'eventlogs':
       input_name   => 'eventlogs',
       input_module => 'im_msvistalog',
-#      query        => '<QueryList><Query Id="0"><Select Path="Application">*</Select><Select Path="Security">*</Select><Select Path="System">*</Select></Query>y</QueryList>',
     }
     nxlog::output {'out':
       output_name   => 'out',
       output_module => 'om_tcp',
-      output_host    => '172.16.20.201',
+      output_host   => $profile_base::monitor_address,
       output_port   => '514',
       output_exec   => 'to_syslog_snare();',
     }
@@ -28,6 +27,6 @@ class profile_base::windows::install {
       route_name => 'route1',
       route_path => 'eventlogs => out',
     }
-#  }
+  }
 
 }
