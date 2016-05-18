53f0cdc3506319e24c5129ff19aec13165e593fd
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-18 15:47:40 +0000

    add desktop shortcut

--- files/bootme.ps1
+++ files/bootme.ps1
@@ -70,6 +70,15 @@
   }
   Write-Host "$puppet_module successfully installed."
 
+  $puppet_module = "puppetlabs/tse-module-shortcut"
+  $puppet_arg = $puppet_modinst + $puppet_module
+  $process = Start-Process -FilePath $puppet_path -ArgumentList $puppet_arg -Wait -PassThru
+  if ($process.ExitCode -ne 0) {
+    Write-Host "Install of $puppet_module failed."
+    Exit 1
+  }
+  Write-Host "$puppet_module successfully installed."
+
   # running puppet apply  
   $puppet_arg = @("apply","-e","`"include $role`"" )
   Write-Host "Running puppet $puppet_arg"
--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -3,4 +3,7 @@
 # This class is called from profile_base for service config.
 #
 class profile_base::windows::config {
+  shortcut { 'C:\Users\Admin\Desktop/myshortcut.lnk':
+    target => 'C:/Windows/system32/cmd.exe',
+  }
 }
