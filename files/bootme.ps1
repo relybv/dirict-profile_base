76bc721ad81a475cce64a38dc0628c935019f679
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-31 23:56:26 +0000

    add puppetlabs/motd

--- files/bootme.ps1
+++ files/bootme.ps1
@@ -79,6 +79,16 @@
   }
   Write-Host "$puppet_module successfully installed."
 
+  $puppet_module = "puppetlabs/motd"
+  $puppet_arg = $puppet_modinst + $puppet_module
+  $process = Start-Process -FilePath $puppet_path -ArgumentList $puppet_arg -Wait -PassThru
+  if ($process.ExitCode -ne 0) {
+    Write-Host "Install of $puppet_module failed."
+    Exit 1
+  }
+  Write-Host "$puppet_module successfully installed."
+
+
   # running puppet apply  
   $puppet_arg = @("apply","-e","`"include $role`"" )
   Write-Host "Running puppet $puppet_arg"
