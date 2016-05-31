91e39073d7b648152cc5087160faf5ec60e810e1
Paul Gomersbach <p.gomersbach@rely.nl>
2016-05-31 23:33:17 +0000

    add motd message

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,7 @@
 # This class is called from profile_base for service config.
 #
 class profile_base::config {
+  class { 'motd':
+    content => 'This host is under control of puppet/nPlease do not change the configuration manual/n',
+  }
 }
--- manifests/windows/config.pp
+++ manifests/windows/config.pp
@@ -3,4 +3,7 @@
 # This class is called from profile_base for service config.
 #
 class profile_base::windows::config {
+  class { 'motd':
+    content => 'This host is under control of puppet/nPlease do not change the configuration manual/n',
+  }
 }
