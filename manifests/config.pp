0ab7057c3f6c2b3f38e7f59494f333c8482d6b17
Paul Gomersbach <p.gomersbach@rely.nl>
2016-07-28 12:21:21 +0000

    save facts provided at provisioning to custom facts file

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,4 +6,21 @@ class profile_base::config {
   class { 'motd':
     content => 'This host is under control of puppet/nPlease do not change the configuration manual/n',
   }
+
+  # save exported facts provided at provisioning to custom facts
+  exec { 'create_fact_file':
+    path    => ['/usr/bin', '/bin'],
+    cwd     => '/opt/puppetlabs/facter/facts.d',
+    command => 'echo \'---\' > fromexport.yaml',
+    creates => '/opt/puppetlabs/facter/facts.d/fromexport.yaml',
+  }
+
+  exec { 'save_facts':
+    path        => ['/usr/bin', '/bin'],
+    cwd         => '/opt/puppetlabs/facter/facts.d',
+    command     => 'export|grep FACTER |cut -d _ -f 2,3 | sed \'s/=/:/g\' | sed \'s/"/ /g\' >> fromexport.yaml',
+    subscribe   => Exec[ 'create_fact_file' ],
+    refreshonly => true,
+  }
+    
 }
