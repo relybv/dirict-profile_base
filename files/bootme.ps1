c871160b59a36ce5e4f4bb069b054e25c63c7643
P. Gomersbach <p.gomersbach@rely.nl>
2016-08-25 08:52:32 +0000

    less modules

--- files/bootme.ps1
+++ files/bootme.ps1
@@ -67,24 +67,6 @@
   }
   Write-Host "$puppet_module successfully installed."
 
-  $puppet_module = "puppetlabs/stdlib"
-  $puppet_arg = $puppet_modinst + $puppet_module
-  $process = Start-Process -FilePath $puppet_path -ArgumentList $puppet_arg -Wait -PassThru -NoNewWindow
-  if ($process.ExitCode -ne 0) {
-    Write-Host "Install of $puppet_module failed."
-    Exit 1
-  }
-  Write-Host "$puppet_module successfully installed."
-
-  $puppet_module = "chocolatey/chocolatey"
-  $puppet_arg = $puppet_modinst + $puppet_module
-  $process = Start-Process -FilePath $puppet_path -ArgumentList $puppet_arg -Wait -PassThru -NoNewWindow
-  if ($process.ExitCode -ne 0) {
-    Write-Host "Install of $puppet_module failed."
-    Exit 1
-  }
-  Write-Host "$puppet_module successfully installed."
-
   $puppet_module = "dschaaff/nxlog"
   $puppet_arg = $puppet_modinst + $puppet_module
   $process = Start-Process -FilePath $puppet_path -ArgumentList $puppet_arg -Wait -PassThru -NoNewWindow
